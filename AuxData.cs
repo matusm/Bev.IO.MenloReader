8121af403dcb2c00052c49359264be624867ddfd
Michael Matus <michael@matus.at>
2020-06-03 22:27:48 +0200

    refactoring

--- AuxData.cs
+++ AuxData.cs
@@ -4,20 +4,18 @@ namespace Bev.IO.MenloReader
 {
     public class AuxData
     {
-        private List<AuxPod> auxList;
+        private readonly List<AuxPod> auxList;
 
-        public AuxPod[] AuxPods { get { return auxList.ToArray(); } }
+        public AuxPod[] AuxPods => auxList.ToArray();
 
         public AuxData(decimal[] x, decimal[] y)
         {
             auxList = new List<AuxPod>();
-
             if (x.Length == y.Length)
             {
                 for (int i = 0; i < x.Length; i++)
                     auxList.Add(new AuxPod(x[i], y[i]));
             }
-
         }
     }
 }
